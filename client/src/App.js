7e010a3fad4cb6a9085bdc09963ae0546709af11
Pardeep Singh <sainiprince424@gmail.com>
2022-03-13 16:28:57 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
